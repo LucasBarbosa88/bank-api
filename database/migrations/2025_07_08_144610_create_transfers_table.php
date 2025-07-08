67558eb971e89fbc3a30c9aabbf8e7fea00011e4
Lucas Barbosa <lucas97ti@gmail.com>
2025-07-08 11:51:28 -0300

    created event controller and migrations for transfer e transactions

---
+++ database/migrations/2025_07_08_144610_create_transfers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransfersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->id();
+            $table->string('origin_id');
+            $table->string('destination_id');
+            $table->decimal('amount', 15, 2);
+            $table->timestamps();
+
+            $table->foreign('origin_id')->references('id')->on('accounts')->onDelete('cascade');
+            $table->foreign('destination_id')->references('id')->on('accounts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transfers');
+    }
+}
