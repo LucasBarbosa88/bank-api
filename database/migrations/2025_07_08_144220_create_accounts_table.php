e40aa1c417b64c37cc699fed4ac91d64a5206745
Lucas Barbosa <lucas97ti@gmail.com>
2025-07-08 11:45:15 -0300

    created project, models, migrations

---
+++ database/migrations/2025_07_08_144220_create_accounts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->string('id')->primary(); // ID da conta (string)
+            $table->decimal('balance', 15, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
