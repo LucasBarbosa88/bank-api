b4e08884c2cfecc791ee8733d06a17284b1fa1e6
Lucas Barbosa <lucas97ti@gmail.com>
2025-08-28 15:18:28 -0300

    change responses from get balance

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Account;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
 
 class AccountController extends Controller
@@ -15,11 +16,15 @@ class AccountController extends Controller
       'account_id' => 'required|integer',
     ]);
 
-    if ($validator->fails()) return response()->json(0, 400);
+    if ($validator->fails()) return response()->json([
+      'error' => 'Account ID is not valid!'
+    ], Response::HTTP_BAD_REQUEST);
 
     $account = Account::find($request->account_id);
 
-    if (!$account) return response()->json(0, 404);
+    if (!$account) return response()->json([
+      'error' => 'Account not found!'
+    ], Response::HTTP_NOT_FOUND);
 
     return response()->json(floatval($account->balance), 200);
   }
